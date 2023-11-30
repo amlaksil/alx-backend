276c26c66fdf312dfe02cb7d0e0b7650a48a26d1
amlaksil <silamlakdesye@gmail.com>
2023-11-30 05:09:48 +0300

    Implement createPushNotificationsJobs function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,32 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData).save((err) => {
+      if (err) {
+        console.error(`Notification job failed: ${err}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+      }
+    });
+
+    job.on('completed', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.error(`Notification job ${job.id} failed: ${err}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+module.exports = createPushNotificationsJobs;
